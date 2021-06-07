f629928a49d4c87bebc1317002b30a3c04441694
EricGiovanni <giovanny.m.t@ciencias.unam.mx>
2021-06-07 17:29:29 -0500

    Agregando archivo base.html y navbar con bootstrap

--- applications/home/views.py
+++ applications/home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import (
     TemplateView,
+    ListView
 )
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
